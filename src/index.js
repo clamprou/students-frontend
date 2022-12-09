449a2595e9acdbad1082d948a1320661c2a08d5a
christosLamp <chrisotelios@gmail.com>
2022-12-09 23:36:38 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import ReactDOM from "react-dom/client";
+
+
+export default function App() {
+    return (
+        <div>
+            <div>
+                <a href='/Register.html'>Register</a>
+            </div>
+            <div>
+                <a href='/Login.html'>Login</a>
+            </div>
+        </div>
+    );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
