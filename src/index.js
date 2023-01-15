2c374441c5e63640979af5ea65bad2154796e5ff
christosLamp <chrisotelios@gmail.com>
2023-01-15 17:58:52 +0200

    Final push hopefully

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import ReactDOM from "react-dom/client";
 export default function App() {
     return (
         <div>
-            <div>
-                <a href='/Register.html'>Register</a>
-            </div>
-            <div>
-                <a href='/Login.html'>Login</a>
-            </div>
-            <div>
-                <a href='/AllCourses.html'>All Courses</a>
-            </div>
+            {/*<div>*/}
+            {/*    <a href='/Register.html'>Register</a>*/}
+            {/*</div>*/}
+            {/*<div>*/}
+            {/*    <a href='/Login.html'>Login</a>*/}
+            {/*</div>*/}
+            {/*<div>*/}
+            {/*    <a href='/AllCourses.html'>All Courses</a>*/}
+            {/*</div>*/}
         </div>
     );
 }
